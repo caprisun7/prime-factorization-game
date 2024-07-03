6d0a5e50486dc9c770a0df5e7bc3c3701864a5a8
caprisun7 <ryanliu@live.com>
2024-07-03 08:13:38 +0800

    pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    basePath: '/prime-factorization-game',
+    assetPrefix: '/prime-factorization-game/',
+  };
+  
+  export default nextConfig;
